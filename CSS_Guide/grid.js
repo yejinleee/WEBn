e1d8cb56e1682b37ef8893ad239f0ea02b2db82a
yejinlee <yejin9487@gmail.com>
2022-01-04 13:04:11 +0900

    grid

--- CSS_Guide/grid.js
+++ CSS_Guide/grid.js
@@ -2,3 +2,7 @@ function gridTemplateColumns(self){
   var target = document.getElementsByClassName('container')[0]
   target.style.gridTemplateColumns=self.value;
 }
+function gridGap(self){
+  var target = document.getElementsByClassName('container')[0]
+  target.style.gap=self.value;
+}
