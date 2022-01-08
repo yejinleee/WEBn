6e4f22e5ede53db48a5b75d647946ec8344d9687
yejinlee <yejin9487@gmail.com>
2022-01-08 22:30:20 +0900

    flex 춘식이고구마

---
+++ CSS_Guide/flex.js
@@ -0,0 +1,12 @@
+function changeJustifycontent(self){
+  var target = document.getElementsByClassName('triangles')[0]
+  target.style.justifyContent=self.value;
+}
+function gridGap(self){
+  var target = document.getElementsByClassName('container')[0]
+  target.style.gap=self.value;
+}
+function changeJustifycontent2(self){
+  var target = document.getElementsByClassName('triangles')[0]
+  target.style.justifyContent=self.value;
+}
