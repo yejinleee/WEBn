d14a119f80abd411b6e8b4720cd461a68703b5a0
yejinlee <yejin9487@gmail.com>
2022-01-12 23:37:48 +0900

    applecoding

---
+++ JS_Practice/apple/indexjs.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName('hello').style.color = "red";
+document.getElementById('hello').style.color = "red";
+document.getElementById("hello").style.fontSize = "30px";
+document.getElementById('hello').innerHTML='ads';
---
+++ JS_Practice/apple/jsjs.js
@@ -0,0 +1,5 @@
+
+function getalert(self){
+  var target = document.getElementsByClassName('alert-box')[0]
+  target.style.display='block';
+}
