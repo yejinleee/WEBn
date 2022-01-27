42f910e8f3e799fe282f4916cc6437e7d8d66232
yejinlee <yejin9487@gmail.com>
2022-01-27 21:20:34 +0900

    painting

---
+++ JS_Practice/canvas/app.js
@@ -0,0 +1,41 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 700;
+canvas.height = 500;
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+
+function onMouseMove(e){
+  // clientX,Y : 윈도우 전체 내에서 마우스 위치값
+  // offsetX,Y : 해당 요소 내에서 마우스 위치값
+  const x = e.offsetX;
+  const y = e.offsetY;
+  if(!painting){
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+  }else{
+    ctx.lineTo(x,y);
+    ctx.stroke();
+  }
+}
+function startPainting(){
+  painting = true;
+}
+function stopPainting(){
+  painting=false;
+}
+function onMousedown(e){
+  painting = true;
+}
+
+
+if(canvas){
+  canvas.addEventListener("mousemove",onMouseMove);
+  canvas.addEventListener("mousedown",startPainting); //마우스'클릭'시 발생
+  canvas.addEventListener("mouseup",stopPainting); //마우스 클릭을 '뗐을 때' 발생
+  canvas.addEventListener("mouseleave",stopPainting);
+
+}
